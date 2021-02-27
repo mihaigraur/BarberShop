0bbffd45bf2b5fd7a612cb2d2b1fc8a2ddd33c88
DESKTOP-7IN0ETU\HomePC <mihaiclau6@gmail.com>
2021-02-27 14:22:33 +0200

    First commit

---
+++ app/src/main/java/com/example/proiectfinal/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.proiectfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.register);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+        });
+
+        Button button1=(Button) findViewById(R.id.login);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+
+    }
+
+    public void openRegister(){
+        Intent intent=new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+    public void openLogin(){
+        Intent intent1=new Intent(this, LoginActivity.class);
+        startActivity(intent1);
+    }
+
+}
